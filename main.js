718a0268a54eb2a09c3b782e4be702717952f406
Jan van Dillen <janvandillen1991@gmail.com>
2017-05-02 01:04:15 +0200

    Builder add

--- main.js
+++ main.js
@@ -45,5 +45,8 @@ module.exports.loop = function () {
         if(creep.memory.role === 'upgrader') {
             roleUpgrader.run(creep);
         }
+        if (creep.memory.role === 'builder') {
+            roleBuilder.run(creep);
+        }
     }
-}
+};
