07dcdc9d64f34d22d8d27deba2992b2685d6e09d
Jan van Dillen <janvandillen1991@gmail.com>
2017-05-04 01:09:54 +0200

    Changes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
+    const spawn = Game.spawns.Spawn1;
+
     if (Game.spawns.Spawn1.energy < 300) {
         if (Game.creeps.Zoe.carry.energy < Game.creeps.Zoe.carryCapacity) {
             if (Game.creeps.Zoe.harvest(Game.getObjectById('58dbc4a48283ff5308a3f585')) === ERR_NOT_IN_RANGE) {
@@ -29,9 +31,19 @@ module.exports.loop = function () {
         Game.spawns.Spawn1.renewCreep(Game.creeps.Owen);
     }
 
-    roleUpgrader.run(Game.creeps.Blake);
-    roleUpgrader.run(Game.creeps.Riley);
-    roleUpgrader.run(Game.creeps.Bella);
+    //roleUpgrader.run(Game.creeps.Blake);
+    //roleUpgrader.run(Game.creeps.Riley);
+    //roleUpgrader.run(Game.creeps.Bella);
+
+    if (spawn.energy === 300 && _.size(Game.creeps) < 12) {
+        spawn.createCreep(Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE, MOVE]), undefined, {role: 'upgrader'});
+    }
+
+    const upgraders = _.filter(Game.creeps, function (creep) {
+        return creep.memory.role === 'upgrader';
+    });
+
+
 
 
 
