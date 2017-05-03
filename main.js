a6662b41ae77b57a247efdac5068d4eea4869d44
Jan van Dillen <janvandillen1991@gmail.com>
2017-05-03 22:18:55 +0200

    Changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 module.exports.loop = function () {
-    if (Game.creeps.Zoe.energy < Game.creeps.Zoe.carryCapacity) {
+    if (Game.creeps.Zoe.carry.energy < Game.creeps.Zoe.carryCapacity) {
         if (Game.creeps.Zoe.harvest(Game.getObjectById('58dbc4a48283ff5308a3f585')) === ERR_NOT_IN_RANGE) {
             Game.creeps.Zoe.moveTo(12, 7);
             Game.creeps.Zoe.say('Hoi');
