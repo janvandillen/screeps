50d9716d29d56951a0119e8102540832994d60ec
Jan van Dillen <janvandillen1991@gmail.com>
2017-05-03 22:04:40 +0200

    Changes

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 module.exports.loop = function () {
-    if (Game.creeps.Zoe.harvest(Game.getObjectById('58dbc4a48283ff5308a3f585')) === ERR_NOT_IN_RANGE) {
-        Game.creeps.Zoe.moveTo(12, 7)
+    if (Game.creeps.Zoe.carry < Game.creeps.Zoe.carryCapacity) {
+        if (Game.creeps.Zoe.harvest(Game.getObjectById('58dbc4a48283ff5308a3f585')) === ERR_NOT_IN_RANGE) {
+            Game.creeps.Zoe.moveTo(12, 7);
+        }
+    } else {
+        Game.creeps.Zoe.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY);
     }
+
+
 };
 
