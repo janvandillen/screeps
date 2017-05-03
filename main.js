3d15327b7e63086826b54290367fa0ef212000ce
Jan van Dillen <janvandillen1991@gmail.com>
2017-05-03 22:21:03 +0200

    Changes

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ module.exports.loop = function () {
     if (Game.creeps.Zoe.carry.energy < Game.creeps.Zoe.carryCapacity) {
         if (Game.creeps.Zoe.harvest(Game.getObjectById('58dbc4a48283ff5308a3f585')) === ERR_NOT_IN_RANGE) {
             Game.creeps.Zoe.moveTo(12, 7);
-            Game.creeps.Zoe.say('Hoi');
         }
-        Game.creeps.Zoe.say('Hi');
+
     } else {
         Game.creeps.Zoe.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY);
-        console.log('Test');
     }
-    console.log('Test2');
-    Game.creeps.Zoe.say('Ho');
+
+    if (Game.creeps.Owen.carry.energy < Game.creeps.Owen.carryCapacity) {
+        if (Game.creeps.Owen.harvest(Game.getObjectById('58dbc4a48283ff5308a3f586')) === ERR_NOT_IN_RANGE) {
+            Game.creeps.Owen.moveTo(13, 7);
+        }
+
+    } else {
+        Game.creeps.Owen.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY);
+    }
+
 
 };
 
