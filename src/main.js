e81fe4d3a8d34b63f57e1bcd57da77b94c9edffa
Jan van Dillen <jan@vandillen.eu>
2020-05-28 20:05:55 +0200

    working start

--- src/main.js
+++ src/main.js
@@ -1,3 +1,41 @@
 module.exports.loop = function () {
-    print("Hello")
+
+    var spawn = Game.spawns.Amsterdam;
+    var target
+
+    if (spawn.energy == spawn.energyCapacity) {
+        target = spawn.room.controller
+    } else {
+        target = spawn
+    }
+
+    if (Object.keys(Game.creeps).length < 5) {
+        spawn.createCreep([WORK, CARRY, MOVE, MOVE], undefined, {working: false, target: target})
+    }
+
+    for (let name in Game.creeps) {
+
+        var creep = Game.creeps[name];
+
+        if (spawn.energy == spawn.energyCapacity) {
+            creep.memory.target = target
+        }
+
+        if (creep.memory.working == true && creep.carry.energy == 0) {
+            creep.memory.working = false
+        } else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.target = target
+            creep.memory.working = true
+        }
+        if (creep.memory.working == true) {
+            if (creep.transfer(Game.getObjectById(creep.memory.target.id), RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.memory.target);
+            }
+        } else {
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+        }
+    }
 }
